a4f7edf72777978313ab7ab424057ac4e90609f0
Arckedo <gardengrade@gmail.com>
2024-01-11 23:34:54 +0100

    correction de bugs

--- code/werewolf.py
+++ code/werewolf.py
@@ -1,8 +1,8 @@
 import random
-import * from roles.py
+from roles import *
 
 class Game:
-    def __init__(self, id_list: list,role_list:list,turn_count:int, player_list = None: list):
+    def __init__(self, id_list: list,role_list:list,turn_count:int, player_list:list = None):
         self.id_list = id_list
         self.player_list = player_list
         self.role_list = role_list
@@ -10,10 +10,11 @@ class Game:
 
     def start(self):
         # Give a class and a Role to each player
-        if player_list == None: 
+        if self.player_list == None: 
             self.player_list = []
-            shuff_id_list = random.shuffle(self.id_list)
-
+            shuff_id_list = list(self.id_list)
+            random.shuffle(shuff_id_list)
+            
             role_class = {
                 "werewolf": Werewolf,
                 "villager": Villager,
@@ -25,7 +26,7 @@ class Game:
 
         for player_id, role in zip(shuff_id_list, self.role_list):
             player_class = role_class[role]
-            player = player_class(id = player_id, self)
+            player = player_class(id = player_id, state = "alive")
             self.player_list.append(player)
             
     def turn(self):
@@ -36,7 +37,16 @@ class Game:
 
 
 
+if __name__ == "__main__":
+    id_list = [1, 2, 3, 4, 5]
+    role_list = ["werewolf", "villager", "seer", "witch", "hunter"]
 
+    game = Game(id_list, role_list, turn_count=10)
+    game.start()
+
+    # Accéder aux joueurs créés
+    for player in game.player_list:
+        print(f"Player {player.id} with role {player.role}")
 
 
 
