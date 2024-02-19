c0a1de8e349c7a6ad2c8444f00c9abe725042268
Arckedo <128933051+Arckedo@users.noreply.github.com>
2024-02-19 20:29:36 +0000

    formatted

--- code/bot/interactions.py
+++ code/bot/interactions.py
@@ -84,7 +84,9 @@ class GarooSelect(Select):
 class GarooUI(View):
     """Représente une interface contenant un ou plusieurs boutons."""
 
-    def __init__(self, *items: Union[GarooButton, GarooSelect], filter: list[int], **kwargs) -> None:
+    def __init__(
+        self, *items: Union[GarooButton, GarooSelect], filter: list[int], **kwargs
+    ) -> None:
         super().__init__(**kwargs)
         self.filter = filter
         self.event: asyncio.Event = None
@@ -95,7 +97,9 @@ class GarooUI(View):
         Retourne si la fonction `callback` associée à l'interaction devrait être appelée ou pas.
         Cette fonction peut être écrasée par des sous-classes."""
         if not interaction.user.id in self.filter:
-            await interaction.response.send_message("❌ Vous n'avez pas le droit d'interagir.", ephemeral=True)
+            await interaction.response.send_message(
+                "❌ Vous n'avez pas le droit d'interagir.", ephemeral=True
+            )
         return False
 
     def get_value(self) -> Any:
@@ -113,7 +117,13 @@ class GarooUI(View):
 class GarooVote(GarooUI):
     """Une sous-classe de GarooUI, configurée pour organiser un vote."""
 
-    def __init__(self,  entries: list[tuple[str, int]], filter: list[int], weight: dict[int, int] = {}, **kwargs) -> None:
+    def __init__(
+        self,
+        entries: list[tuple[str, int]],
+        filter: list[int],
+        weight: dict[int, int] = {},
+        **kwargs
+    ) -> None:
         """
         Paramètres
         ----------
@@ -128,7 +138,7 @@ class GarooVote(GarooUI):
         # [self.message.guild.get_member(id).nick for id in entries]
         options = [SelectOption(label=str(name), value=str(id)) for name, id in entries]
         entries_names = [name for name, id in entries]
-        entries_id = [id for name,id in entries]
+        entries_id = [id for name, id in entries]
         print(entries_names)
         super().__init__(Select(options=options), filter=filter, **kwargs)
         self.weight = weight
@@ -140,18 +150,23 @@ class GarooVote(GarooUI):
 
         # Si l'utilisateur n'est pas dans la liste des autorisés
         if user_id not in self.filter:
-            await interaction.response.send_message("❌ Vous n'avez pas le droit de voter.", ephemeral=True)
+            await interaction.response.send_message(
+                "❌ Vous n'avez pas le droit de voter.", ephemeral=True
+            )
 
         # Si l'utilisateur n'a pas encore voté
         elif user_id not in self.voted_list:
             entry = int(self.children[0].values[0])
             self.voted_list.append(user_id)
-            self.votes[entry] += (
-                self.weight.get(user_id, 1))
-            await interaction.response.send_message("✅ Vous avez voté.", ephemeral=True)
+            self.votes[entry] += self.weight.get(user_id, 1)
+            await interaction.response.send_message(
+                "✅ Vous avez voté.", ephemeral=True
+            )
 
         else:
-            await interaction.response.send_message("❌ Vous avez déjà voté.", ephemeral=True)
+            await interaction.response.send_message(
+                "❌ Vous avez déjà voté.", ephemeral=True
+            )
 
         # Si tous les utilisateurs de la liste ont voté
         if len(self.voted_list) >= len(self.filter):
@@ -184,12 +199,16 @@ class GarooChoose(GarooUI):
     async def interaction_check(self, interaction: Interaction) -> bool:
         if interaction.user.id in self.filter:
             # Si l'utilisateur est dans la liste des autoriés
-            await interaction.response.send_message("✅ Vous avez choisi.", ephemeral=True)
+            await interaction.response.send_message(
+                "✅ Vous avez choisi.", ephemeral=True
+            )
             # Termine l'intraction
             self.event.set()
         else:
             # Si l'utilisateur n'est pas dans la liste des autaurisés
-            await interaction.response.send_message("❌ Vous n'avez pas le droit d'interagir.", ephemeral=True)
+            await interaction.response.send_message(
+                "❌ Vous n'avez pas le droit d'interagir.", ephemeral=True
+            )
         return False
 
     def get_value(self) -> str:
@@ -203,7 +222,7 @@ class GarooClient:
         self,
         client: Client,
         channel: TextChannel,
-        werewolf_channel: Optional[TextChannel] = None
+        werewolf_channel: Optional[TextChannel] = None,
     ) -> None:
         """
         Paramètres
@@ -232,7 +251,7 @@ class GarooClient:
         `Thread`
             Le salon créé.
         """
-        channel =await self.channel.create_thread(name="loups-garous")
+        channel = await self.channel.create_thread(name="loups-garous")
         self.werewolf_channel = channel
         for player in werewolf_ids:
             await channel.add_user(self.get_user(player))
@@ -251,9 +270,16 @@ class GarooClient:
             L'utilisateur associé à l'identifiant.
             Renvoie `None` si l'identifiant est invalide.
         """
-        return self.client.loop.run_until_complete(self.client.get_or_fetch_user(user_id))
+        return self.client.loop.run_until_complete(
+            self.client.get_or_fetch_user(user_id)
+        )
 
-    def send(self, content: str= None,embed: GarooEmbed = None, dest: Union[TextChannel, User] = None) -> None:
+    def send(
+        self,
+        content: str = None,
+        embed: GarooEmbed = None,
+        dest: Union[TextChannel, User] = None,
+    ) -> None:
         """Envoie un message.
 
         Paramètres
@@ -265,15 +291,29 @@ class GarooClient:
             envoyé dans le salon `channel` lié à l'objet.
         """
         dest = dest or self.channel
-        self.client.loop.run_until_complete(dest.send(content=content,embed=embed))
+        self.client.loop.run_until_complete(dest.send(content=content, embed=embed))
 
-    async def __send_interface(self, content: str = None,*, embed : GarooEmbed = None, view: GarooUI, dest: Union[TextChannel, User]) -> None:
+    async def __send_interface(
+        self,
+        content: str = None,
+        *,
+        embed: GarooEmbed = None,
+        view: GarooUI,
+        dest: Union[TextChannel, User]
+    ) -> None:
         event = asyncio.Event()
         view.setup_event(event)
-        asyncio.create_task(dest.send(content,embed=embed, view=view))
+        asyncio.create_task(dest.send(content, embed=embed, view=view))
         await event.wait()
 
-    def send_interface(self, content: str = None, embed:GarooEmbed = None,*, interface: GarooUI, dest: Union[TextChannel, User] = None) -> None:
+    def send_interface(
+        self,
+        content: str = None,
+        embed: GarooEmbed = None,
+        *,
+        interface: GarooUI,
+        dest: Union[TextChannel, User] = None
+    ) -> None:
         """Envoie un message contenant une interface.
 
         Paramètres
@@ -292,6 +332,8 @@ class GarooClient:
             La valeur associée à l'interface une fois l'interaction achevée.
         """
         dest = dest or self.channel
-        task = self.__send_interface(content = content,embed= embed,view = interface,dest= dest)
+        task = self.__send_interface(
+            content=content, embed=embed, view=interface, dest=dest
+        )
         self.client.loop.run_until_complete(task)
         return interface.get_value()
