b4c4b6873b358478f134bcffef4aae4c086376a1
Arckedo <128933051+Arckedo@users.noreply.github.com>
2024-02-27 18:28:01 +0000

    debug witch 2

--- code/main_game.py
+++ code/main_game.py
@@ -276,7 +276,7 @@ class Game:
         self.game_embed(
             title=f"🌅 __Jour {self.turn_count}__ 🌅",
             thumbnail={"url": "https://th.bing.com/th/id/OIG2.OColV0JanmsfatOIhZge"},
-            description=f"L'aube éblouit Thiercelieux, Les joueurs suivant sont morts cette nuit : {", ".join(f"{self.mention(player)} : {self.find_role(player)}" for player in death)}",
+            description=f"""L'aube éblouit Thiercelieux, Les joueurs suivant sont morts cette nuit : {", ".join(f"{self.mention(player)} : {self.find_role(player)}" for player in death)}""",
         )
 
         if self.mayor_id in death:
